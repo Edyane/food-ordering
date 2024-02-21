141a2f6c32e21691bc125ee4ab24a5b3970a8908
Edyane <edyanealves@gmail.com>
2024-02-20 22:32:34 -0300

    feat: importando componentes

--- food-ordering/src/app/page.js
+++ food-ordering/src/app/page.js
@@ -1,7 +1,13 @@
+import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
+
 export default function Home() {
 	return (
-		<div>
-			<h1>Teste</h1>
-		</div>
+		<>
+			<Header />
+			<Hero />
+			<HomeMenu />
+		</>
 	);
 }
