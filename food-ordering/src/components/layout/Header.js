dafb6d23d0cdfe695200318f54c0e90d763f7288
Edyane <edyanealves@gmail.com>
2024-02-26 22:23:02 -0300

    feat: adicionando ajustes ao componente Header

--- food-ordering/src/components/layout/Header.js
+++ food-ordering/src/components/layout/Header.js
@@ -1,26 +1,26 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Home() {
+export default function Header() {
 	return (
 		<>
-			<header className="flex items-center justify-between">
-				<Link href={""} className="text-primary font-semibold text-2xl">
+			<header className="flex items-center justify-between mt-4">
+				<nav className="flex items-center gap-8 text-gray-500 font-semibold">
+				<Link href={'/'} className="text-primary font-semibold text-2xl">
 					ST PIZZA
 				</Link>
-
-				<nav className="flex items-center gap-8 text-gray-500 font-semibold">
-					<Link href={""}>Home</Link>
+					<Link href={'/'}>Home</Link>
 					<Link href={""}>Menu</Link>
 					<Link href={""}>About</Link>
 					<Link href={""}>Contact</Link>
-					<Link
-						href={""}
-						className="bg-primary rounded-full text-white px-8 py-2"
-					>
-						Login
+				</nav>
+
+				<nav className="flex items-center gap-4 text-gray-500 font-semibold">
+					<Link href={'/login'}>Login</Link>
+					<Link href={'/register'} className="bg-primary rounded-full text-white px-8 py-2">
+						Register
 					</Link>
 				</nav>
 			</header>
 		</>
 	);
-};
+}
