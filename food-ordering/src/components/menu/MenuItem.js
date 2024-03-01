90bbcc56cddfd57671acf63fa7aa4a26d3791f88
Edyane <edyanealves@gmail.com>
2024-02-29 22:23:42 -0300

    fix: adicionando pequeno ajute ao MenuItem

--- food-ordering/src/components/menu/MenuItem.js
+++ food-ordering/src/components/menu/MenuItem.js
@@ -1,7 +1,13 @@
 export default function MenuItem() {
 	return (
 		<div className="bg-gray-200 p-4 rounded-lg text-center group hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all">
-			<img src="/pizza.png" alt="pizza" />
+			<div className="text-center">
+				<img
+					src="/pizza.png"
+					className="max-h-auto max-h-24 block mx-auto"
+					alt="pizza"
+				/>
+			</div>
 			<h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
 			<p className="text-gray-500 text-sm">lorem ipsum dolor sit amet</p>
 			<button className="mt-4 bg-primary text-white rounded-full px-8 py-2">
@@ -9,4 +15,4 @@ export default function MenuItem() {
 			</button>
 		</div>
 	);
-};
+}
