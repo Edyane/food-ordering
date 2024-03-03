e6e9fc6827b2135ba213ffa6c1b5c7cb5caee241
Edyane <edyanealves@gmail.com>
2024-03-02 22:50:02 -0300

    fix: adicionando ajustes de layout

--- food-ordering/src/app/layout.js
+++ food-ordering/src/app/layout.js
@@ -1,11 +1,8 @@
+import Header from "@/components/layout/Header";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/layout/Header";
 
-const roboto = Roboto({
-	subsets: ["latin"],
-	weight: ["100", "300", "400", "500", "700", "900"],
-});
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
 	title: "Create Next App",
@@ -14,15 +11,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" className="scroll-smooth">
 			<body className={roboto.className}>
-				<main className="max-w-4xl mx-auto">
+				<main className="max-w-4xl mx-auto p-4">
 					<Header />
 					{children}
+					<footer className="border-t p-8 text-center text-gray-500 mt-16">
+						&copy; 2023 All rights reserved
+					</footer>
 				</main>
-				<footer className="border-t p-8 text-center text-gray-500">
-					&copy; 2024 All rights reserved
-				</footer>
 			</body>
 		</html>
 	);
