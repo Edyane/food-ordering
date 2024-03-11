2075a001ea973af122189ca2e6aeacdc3b087c19
Edyane <edyanealves@gmail.com>
2024-03-11 20:40:20 -0300

    feat: construindo handleFormSubmit function

--- food-ordering/src/app/login/page.js
+++ food-ordering/src/app/login/page.js
@@ -5,10 +5,19 @@ export default function LoginPage() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
+	async function handleFormSubmit(e) {
+		e.preventDefault();
+		await fetch('/api/login', { 
+			body: JSON.stringify({email, password}),
+			headers: {'Content-Type': 'application/json'},
+			method: 'POST',
+		})
+	}
+
 	return (
 		<section className="mt-8">
 			<h1 className="text-center text-primary text-4xl mb-4">Login</h1>
-			<form className="block max-w-xs mx-auto">
+			<form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
 				<input
 					type="email"
 					placeholder="Email"
@@ -25,6 +34,18 @@ export default function LoginPage() {
 				/>
 				<button type="submit" disabled={false}>
 					Login
+				</button>
+				<div className="my-4 text-center text-gray-500">
+					or login with provider
+				</div>
+				<button className="flex gap-4 justify-center">
+					<Image
+						src={"/google.png"}
+						alt={""}
+						width={24}
+						height={24}
+					/>
+					Login with Google
 				</button>
 			</form>
 		</section>
