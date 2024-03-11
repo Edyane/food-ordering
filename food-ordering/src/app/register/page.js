eb1ce7b458c6ef0a90ea76ba3cf7610a19ddfb35
Edyane <edyanealves@gmail.com>
2024-03-10 22:51:31 -0300

    feat: adicionando verificações na página de registro

--- food-ordering/src/app/register/page.js
+++ food-ordering/src/app/register/page.js
@@ -1,28 +1,55 @@
 "use client";
 import Image from "next/image";
-import { useState } from 'react';
+import Link from "next/link";
+import { useState } from "react";
 
 export default function RegisterPage() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [creatingUser, setCreatingUser] = useState(false);
 	const [userCreated, setUserCreated] = useState(false);
+	const [error, setError] = useState(false);
 
-	async function handleFormSubmit (e) {
+	async function handleFormSubmit(e) {
 		e.preventDefault();
 		setCreatingUser(true);
-		await fetch('/api/register', {
-			method: 'POST',
-			body: JSON.stringify({email, password}),
-			headers: {'Content-Type' : 'application/json'},
+		setError(false);
+		setUserCreated(false);
+		const response = await fetch("/api/register", {
+			method: "POST",
+			body: JSON.stringify({ email, password }),
+			headers: { "Content-Type": "application/json" },
 		});
+		if (response.ok) {
+			setUserCreated(true);
+		} else {
+			setError(true);
+		}
 		setCreatingUser(false);
-	};
+	}
 
 	return (
 		<section className="mt-8">
 			<h1 className="text-center text-primary text-4xl mb-4">Register</h1>
-			<form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+			{userCreated && (
+				<div className="my-4 text-center">
+					User created.<br></br>
+					<Link className="underline" href={"/login"}>
+						Click here to Login
+					</Link>
+				</div>
+			)}
+
+			{error && (
+				<div className="my-4 text-center">
+					<p>An error has occurred. Please try again.</p>
+				</div>
+			)}
+
+			<form
+				className="block max-w-xs mx-auto"
+				onSubmit={handleFormSubmit}
+			>
 				<input
 					type="email"
 					placeholder="Email"
@@ -37,7 +64,9 @@ export default function RegisterPage() {
 					onChange={(e) => setPassword(e.target.value)}
 					disabled={creatingUser}
 				/>
-				<button type="submit">Register</button>
+				<button type="submit" disabled={creatingUser}>
+					Register
+				</button>
 
 				<div className="my-4 text-center text-gray-500">
 					or login with provider
@@ -51,6 +80,10 @@ export default function RegisterPage() {
 					/>
 					Login with Google
 				</button>
+				<div className="text-center my-4 text-gray-500 pt-4">
+					<span className="px-1">Existing account?</span> 
+					<Link className="underline text-primary" href={'/login'}>Login here</Link>
+				</div>
 			</form>
 		</section>
 	);
