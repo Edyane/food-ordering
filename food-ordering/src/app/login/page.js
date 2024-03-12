53662ccd0c95fce4f9194163d13a2c0f3907062c
Edyane <edyanealves@gmail.com>
2024-03-11 21:32:09 -0300

    feat: ajustando autenticação

--- food-ordering/src/app/login/page.js
+++ food-ordering/src/app/login/page.js
@@ -1,44 +1,51 @@
 "use client";
+import { sign } from "crypto";
 import { useState } from "react";
 
 export default function LoginPage() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [loginInProgress, setLoginInProgress] = useState(false);
 
 	async function handleFormSubmit(e) {
 		e.preventDefault();
-		await fetch('/api/login', { 
+		setLoginInProgress(true);
+		const {ok} = await fetch('/api/login', { 
 			body: JSON.stringify({email, password}),
 			headers: {'Content-Type': 'application/json'},
 			method: 'POST',
-		})
+		});
+		await Signika_Negative('Credentials')
+		setLoginInProgress(false);
 	}
 
 	return (
 		<section className="mt-8">
 			<h1 className="text-center text-primary text-4xl mb-4">Login</h1>
-			<form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+			<form className="block max-w-xs mx-auto" method="POST" action="/api/auth">
 				<input
 					type="email"
+					name="email"
 					placeholder="Email"
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
-					disabled={false}
+					disabled={loginInProgress}
 				/>
 				<input
 					type="password"
+					name="password"
 					placeholder="Password"
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
-					disabled={false}
+					disabled={loginInProgress}
 				/>
-				<button type="submit" disabled={false}>
+				<button type="submit" disabled={loginInProgress}>
 					Login
 				</button>
 				<div className="my-4 text-center text-gray-500">
 					or login with provider
 				</div>
-				<button className="flex gap-4 justify-center">
+				<button disabled={false} className="flex gap-4 justify-center">
 					<Image
 						src={"/google.png"}
 						alt={""}
