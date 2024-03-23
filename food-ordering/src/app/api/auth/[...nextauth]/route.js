ea3753a65f1591ebae1a147275fdddcc56d37036
Edyane <edyanealves@gmail.com>
2024-03-22 22:24:10 -0300

    feat: verificando credenciais de email e senha

--- food-ordering/src/app/api/auth/[...nextauth]/route.js
+++ food-ordering/src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,13 @@
+import * as mongoose from "mongoose";
+import {User} from '@models/User';
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
+	secret: process.env.SECRET,
 	providers: [
 		CredentialsProvider({
-			name: "Credentials",
+			name: 'Credentials',
 			id: 'credentials',
 			credentials: {
 				username: {
@@ -15,10 +18,24 @@ const handler = NextAuth({
 				password: { label: "Password", type: "password" },
 			},
 			async authorize(credentials, req) {
-				console.log({credentials})
+				const email = credentials?.email;
+				const password = credentials?.password;
+
+				mongoose.connect(process.env.MONGO_URL);
+				const user = await User.findOne({email});
+				const passwordOk = user && bcrypt.compareSync(password, user.password); 
+
+				console.log({passwordOk});
+
+				if (passwordOk) {
+					return user;
+				}
+
 				return null;
 			},
 		}),
 	],
 });
+
+export { handler as GET, handler as POST}
 
