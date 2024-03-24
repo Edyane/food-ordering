fdf04f70ecfbb725b3af7c95eac01140621b154f
Edyane <edyanealves@gmail.com>
2024-03-24 14:48:13 -0300

    feat: adicionando verificação e logout

--- food-ordering/src/components/layout/Header.js
+++ food-ordering/src/components/layout/Header.js
@@ -1,24 +1,43 @@
+"use client"
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Header() {
+	const session = useSession();
+	console.log(session);
+	const status = session?.status;
+
 	return (
 		<>
 			<header className="flex items-center justify-between mt-4">
 				<nav className="flex items-center gap-8 text-gray-500 font-semibold">
-				<Link href={'/'} className="text-primary font-semibold text-2xl">
-					ST PIZZA
-				</Link>
-					<Link href={'/'}>Home</Link>
+					<Link href={"/"} className="text-primary font-semibold text-2xl">
+						ST PIZZA
+					</Link>
+					<Link href={"/"}>Home</Link>
 					<Link href={""}>Menu</Link>
 					<Link href={""}>About</Link>
 					<Link href={""}>Contact</Link>
 				</nav>
 
 				<nav className="flex items-center gap-4 text-gray-500 font-semibold">
-					<Link href={'/login'}>Login</Link>
-					<Link href={'/register'} className="bg-primary rounded-full text-white px-8 py-2">
-						Register
-					</Link>
+					{status === "authenticated" && (
+						<button href={"/register"} 
+							onClick={() => signOut()}
+							className="bg-primary rounded-full text-white px-8 py-2">
+							Register
+						</button>
+					)}
+
+					{status !== 'authenticated' && (
+						<>
+							<Link href={"/login"}>Login</Link>
+							<Link href={"/register"} className="bg-primary rounded-full text-white px-8 py-2">
+								Register
+							</Link>
+						</>
+					)}
+					
 				</nav>
 			</header>
 		</>
